e6e5b623c71c8b184e7cb20607f2a82cf738e002
priyulp <93876688+priyulp@users.noreply.github.com>
2025-03-19 17:03:27 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket"
+    bucket         = "terraform-state-priyu"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
